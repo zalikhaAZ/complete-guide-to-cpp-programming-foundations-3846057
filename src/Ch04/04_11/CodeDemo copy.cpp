6cf9e5c149620d830508ce032ceba6e3b195a074
zalikhaAZ <zalikhazainudin@gmail.com>
2025-07-03 06:16:14 +0000

    modified

---
+++ src/Ch04/04_11/CodeDemo copy.cpp
@@ -0,0 +1,32 @@
+// Complete Guide to C++ Programming Foundations
+// Challenge 04_11
+// Vector Manipulation, by Eduardo Corpeño 
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::vector<int> SelectKeyPoints(std::vector<int> distances, int checkpointIndex){
+    std::vector<int> result;
+
+    result = distances;
+    
+    return result;
+}
+
+int main(){
+    std::vector<int> distances = {3, 5, 4, 6, 7, 8};
+    int checkpointIndex = 4;
+
+    std::vector<int> learnerResult = SelectKeyPoints(distances, checkpointIndex);
+    std::cout << "Your code returned: { ";
+    auto vectPtr = learnerResult.begin();
+    while (vectPtr != learnerResult.end()){
+        std::cout << *vectPtr << " ";
+        vectPtr = next(vectPtr, 1);
+    }
+    std::cout << "}" << std::endl;
+    
+    std::cout << std::endl << std::endl;
+    return 0;
+}
