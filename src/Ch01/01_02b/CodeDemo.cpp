7d44c992a1acf98cf4546cb72cabe288b038fac9
zalikhaAZ <zalikhazainudin@gmail.com>
2025-08-28 05:48:29 +0000

    modified

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    std::cout <<"have a nice day!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return 0;
+}
