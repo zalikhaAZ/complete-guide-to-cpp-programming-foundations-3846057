16bff5cdcd5d963d869c34de672ed22f8f4a11f7
zalikhaAZ <zalikhazainudin@gmail.com>
2025-09-24 06:47:04 +0000

    modified

--- src/Ch07/07_02b/CodeDemo.cpp
+++ src/Ch07/07_02b/CodeDemo.cpp
@@ -5,6 +5,54 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
+
+class Inventory{
+public:
+    // add item to inventory
+    void addItem(const std::string& item){
+        if (items->size() < capacity)
+            items->push_back(item);
+        else
+            std::cout << "Inventory is full, cannot add" << item << std::endl;
+    }
+
+    // remove item from inventory
+    void removeItem(const std::string& item){
+        auto it = std::find(items->begin(), items->end(), item);
+        if (it != items->end())
+            items->erase(it);
+        else
+            std::cout << "Item " << item << " not found in inventory" << std::endl;
+    }
+
+    // Access item by index
+    std::string getItem(int index) const{
+        if (index <= 0 && index < items->size())
+            return (*items)[index];
+        else
+            return "Index out of bounds";
+    }
+
+    // Get number of items in inventory
+    int getItemCount() const{
+        return items->size();
+    }
+
+    // Display inventory contents
+    void displayInventory() const{
+        std::cout << "Inventory: [ ";
+        for (size_t i = 0; i < items ->(); i++){
+            std::cout << (*items)[i];
+            if (i < items->size() - 1) std::cout << ", ";
+        }
+        std::cout << " ]" << std::endl;
+    }
+
+private:
+    std::vector<std::string> *items; // pointer to a vector of items
+    int capacity; // maximum number of items allowed
+};
 
 int main(){
     
